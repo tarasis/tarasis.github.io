4eab3313a10296965b9bf21864d23d9469ef3c6a
Robert McGovern <rob@tarasis.net>
2020-10-04 00:34:27 +0100

    Sharing working, desktop and mobile are close. Haven't matched design for sharing.

---
+++ FrontendMentor/article-preview-component/js/challenge.js
@@ -0,0 +1,38 @@
+let isTooltipVisible = false;
+
+function toggleTooltip() {
+    const articleFooter = document.querySelector(".articleFooter");
+    const articleImage = document.querySelector(".authorImage");
+    const articleAuthorAndDate = document.querySelector(".nameAndDate");
+    const articleShareButton = document.querySelector(".articleShareButton");
+    const articleShareTooltip = document.querySelector(".articleShareButtonTooltip");
+    const articleShareSection = document.querySelector(".articleShareSection");
+
+    if (window.innerWidth <= 500) {
+        if (isTooltipVisible) {
+            articleFooter.classList.remove("backgroundDark");
+            articleShareButton.classList.remove("buttonDark");
+            articleShareSection.style.display = "none";
+            articleImage.style.display = "block";
+            articleAuthorAndDate.style.display = "block";
+            isTooltipVisible = false;      
+        } else {
+            articleFooter.classList.add("backgroundDark");
+            articleShareButton.classList.add("buttonDark");
+            articleShareSection.style.display = "flex";
+            articleImage.style.display = "none";
+            articleAuthorAndDate.style.display = "none";
+            isTooltipVisible = true;      
+        }
+    } else {
+        if (isTooltipVisible) {
+            articleShareButton.classList.remove("buttonDark");
+            articleShareTooltip.style.display = "none";
+            isTooltipVisible = false;    
+        } else {
+            articleShareButton.classList.add("buttonDark");
+            articleShareTooltip.style.display = "flex";
+            isTooltipVisible = true;
+        }
+    }
+}
--- FrontendMentor/article-preview-component/notes/process.md
+++ FrontendMentor/article-preview-component/notes/process.md
@@ -32,3 +32,17 @@ Still trying to eyeball what I can but I find myself wanting to look (for line a
 Did use it for the shadow, because I wasn't going to guess that color. Sheesh 🤯
 
 Picking this up on the 3rd. The mobile version is nearly properly positioned. Its off by a bit (whole component is a little shorter), but everything else is close. (Except still not sorted the thing around the share icon) 
+
+---
+
+Can't figure out how to properly portion to get the desktop image a bit bigger. Playing with flex portions changes too much. Grid better? I want to say the pic is 40% of the size, and body is 60% of the size.
+
+---
+
+Picked up hours later.
+
+Used this https://cssdeck.com/labs/bv45bh6p for the callout/tooltip
+
+Don't understand why the img/svg inside the button shifts up when tooltip appears.
+
+Broke the desktop version while adding the share stuff. Not entirely sure how.
