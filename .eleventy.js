5aa2a43fbe8ee521c70e4f527d535dc570a9f2cc
Robert McGovern <rob@tarasis.net>
2022-02-05 13:48:21 +0000

    further setup. Base site isn't using eleventy directly yet. Not sold on how I've set folders up at the moment. Need to rearrange but not sure if I want svgs images fonts css in assets or moved up a level to the top level where src and www are

---
+++ .eleventy.js
@@ -0,0 +1,24 @@
+module.exports = function (eleventyConfig) {
+    eleventyConfig.addPassthroughCopy("./src/css");
+    eleventyConfig.addPassthroughCopy("./src/fonts");
+    eleventyConfig.addPassthroughCopy("./src/images");
+    eleventyConfig.addPassthroughCopy("./src/svgs");
+    eleventyConfig.addPassthroughCopy("./src/js");
+    //eleventyConfig.addPassthroughCopy({
+    //    "./src/assets/images": "img",
+    //});
+
+    // Return your Object options:
+    return {
+        dir: {
+            input: "src",
+            output: "www",
+            // ⚠️ These values are both relative to the input directory.
+            // 📝 _includes is a default value, as is _data but I like them
+            //      visible here as a reminder
+            includes: "includes",
+            layouts: "layouts",
+            data: "data",
+        },
+    };
+};
--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # rmcg.dev
 
 Projects completed while working through various HTML/CSS/JavaScript things. Primarily focused on Frontend Mentor. Ultimately it will aim to be a portfolio site.
+
+Fedora and other SVG's came from
+
+https://freesvg.org/red-fedora-reworked
+
+License is public domain for the nicer of the Fedoras
+
+Added basic website 5th Feb 2022
+
+--
+
+Self hosting repo on git.tarasis.net
+Set up post-receive hook on gitea to deploy the update for caddy
