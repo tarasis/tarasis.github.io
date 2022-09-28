86a559963edffeae14c047ea1fadb2eea8a8a4cf
Robert McGovern <rob@tarasis.net>
2022-09-28 21:06:27 +0100

    finished javascript of intro challenge

--- projects/FrontendMentor/newbie/intro-component-with-signup-form/js/script.js
+++ projects/FrontendMentor/newbie/intro-component-with-signup-form/js/script.js
@@ -1,11 +1,39 @@
-function verifyData(form) {
-    console.log(typeof form);
+"use strict";
 
-    return false;
+const form = document.getElementById("details-form");
+const inputs = document.querySelectorAll("input");
+
+for (const input of inputs) {
+    // console.log(`${input.id} getting focus listener`)
+    input.addEventListener("focus", inputFocused, false);
+}
+form.addEventListener("submit", checkFormValidity, false);
+
+/**
+ * @param {Event} event
+ */
+function checkFormValidity(event) {
+    if (form.checkValidity() === false) {
+        event.preventDefault();
+        const invalids = form.querySelectorAll(":invalid");
+        for (const input of invalids) {
+            // console.log(`${input.id} is invalid`);
+            // console.log(`${input.id} msg ${input.validationMessage}`);
+            // get the relevant error message element, using id of the input
+            let errorSection = document.getElementById(`errorMessage-${input.id}`)
+            // console.log(`ES: ${errorSection}, ${typeof (errorSection)} id: ${errorSection.id}`);
+            errorSection.removeAttribute("hidden");
+            input.classList.add("error-highlight")
+        }
+    }
 }
 
-function validateForm() {
-    console.log("Hello");
-
-    return false;
+// When an input has focus I remove the error state,
+// I could more ideally check if the class is in the classlist,
+// and attribute hidden was false
+function inputFocused(event) {
+    const input = event.currentTarget;
+    input.classList.remove("error-highlight");
+    let errorSection = document.getElementById(`errorMessage-${input.id}`);
+    errorSection.setAttribute("hidden", true);
 }
