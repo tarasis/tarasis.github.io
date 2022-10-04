4f87fa5031a90a02590a00e57be19b2573928169
Robert McGovern <rob@tarasis.net>
2022-10-04 13:08:30 +0100

    styling for mobile & desktop versions finished(?)

---
+++ projects/FrontendMentor/junior/advice-generator-app/js/script.js
@@ -0,0 +1,6 @@
+// api used is at https://api.adviceslip.com
+// specifically https://api.adviceslip.com/advice
+// which takes a callback parameter
+// advice is cached for 2 seconds
+// so my plan is to disable the button for those
+// 2 seconds
