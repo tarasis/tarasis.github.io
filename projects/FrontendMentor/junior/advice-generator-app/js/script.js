7358c8288f8b943da66d27fc3a5ab2d96bfa8cd2
Robert McGovern <rob@tarasis.net>
2022-10-04 14:35:30 +0100

    finished javascript, submitting advice-generator-app

--- projects/FrontendMentor/junior/advice-generator-app/js/script.js
+++ projects/FrontendMentor/junior/advice-generator-app/js/script.js
@@ -4,3 +4,54 @@
 // advice is cached for 2 seconds
 // so my plan is to disable the button for those
 // 2 seconds
+"use strict";
+
+const title = document.getElementById("advice__title");
+const quote = document.getElementById("advice__quote");
+const button = document.getElementById("advice__button");
+
+button.addEventListener("click", getAdvice);
+
+window.document.addEventListener("DOMContentLoaded", getAdvice);
+
+/**
+ * [buttonClicked description]
+ *
+ * @param   {[Event]}  anEvent  [anEvent description]
+ */
+async function getAdvice() {
+    try {
+        addAnimation();
+
+        const response = await fetch("https://api.adviceslip.com/advice");
+        const data = await response.json();
+
+        // verify its a slip
+        if (data.slip) {
+            title.textContent = `Advice #${data.slip.id}`;
+            quote.textContent = `“${data.slip.advice}”`;
+        } else {
+            // If for some reason the server sends a message type
+            // back rather than a slip
+            alert(`Something went wrong.\n\n${data.message.text}`);
+
+            removeAnimation();
+        }
+        // otherwise something horrible went wrong
+    } catch (error) {
+        alert(`Something has gone seriously wrong. HELP!!!!!\n\n ${error}`);
+
+        removeAnimation();
+    }
+}
+
+function addAnimation() {
+    button.children[0].classList.add("animate");
+    setTimeout(() => {
+        removeAnimation();
+    }, 2000);
+}
+
+function removeAnimation() {
+    button.children[0].classList.remove("animate");
+}
