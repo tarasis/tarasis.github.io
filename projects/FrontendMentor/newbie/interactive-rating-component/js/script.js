eb4b2dbefbb5851bc8cb4de137a6038264afb96c
Robert McGovern <rob@tarasis.net>
2022-10-27 22:52:21 +0100

    finished back of card for mobile, added blur to javascript to remove the focus effect

--- projects/FrontendMentor/newbie/interactive-rating-component/js/script.js
+++ projects/FrontendMentor/newbie/interactive-rating-component/js/script.js
@@ -1,5 +1,6 @@
 const form = document.getElementById("ratingForm");
 const ratingSpan = document.getElementById("ratingSpan");
+const buttonSubmit = document.getElementById("buttonSubmit");
 
 const frontSection = document.getElementById("rating-component-front");
 const backSection = document.getElementById("rating-component-back");
@@ -17,8 +18,18 @@ form.addEventListener(
         ratingSpan.innerText = output;
 
         //hide first section, make second section visible
-        frontSection.classList.add("hidden");
-        backSection.classList.remove("hidden");
+        if (output != "") {
+            frontSection.classList.add("hidden");
+            backSection.classList.remove("hidden");
+        } else {
+            /*
+            Blur is used to remove the focus from the button that
+            we prevent from working because the user didn't select
+            a rating. Should probably add an error message, even
+            though its not in the design spec.
+            */
+            buttonSubmit.blur();
+        }
         //use a transition effect, plus a reduced motion version
 
         event.preventDefault();
